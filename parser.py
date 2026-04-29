7e280a55fe22bbc1091a4451964c8dc16eb2f456
EllynaSoumelis <ellyna@gmx.fr>
2026-04-29 13:50:11 +0200

    mdif q10

--- parser.py
+++ parser.py
@@ -66,76 +66,92 @@ if __name__ == '__main__':
 	c = x.create_init_config("1001010100")
 	print(x.next_step(c))
 
-def conversion_binaire(number):
+def binary_conversion(number, nb_bits):
 	"""Function that ables to convert decimal numbers in binary numbers
 	Parameter : number which represents the decimal number"""
 	reste = []
 
-	if number == 0:
+	if number == 0 and nb_bits is None:
 		return '0'
 	
-	while number//2 > 0 :
-		reste.append(number%2)
-		number = number//2
-	reste.reverse()
+	binary = bin(number)[2:]
+	
+	if nb_bits is not None :
+		if len(binary) > nb_bits : 
+			raise ValueError(f"{number} cannot fit in {nb_bits} bits")
+		return binary.zfill(nb_bits)
+	return binary
+	
 
-	return ''.join(str(b) for b in reste)
-
-
-def rename_states(machine) :
+def rename_states(machine, nb_bits = 8) :
 	"""Renames all the states with a binary number instead
 	We force the naming of the initial state and the accept state to 0 and 1 each
 	Parameters : machine which represents the turing machine we want to convert
 	"""
+	max_states = 2 **nb_bits
+	if len(machine.states) > max_states :
+		raise ValueError(f"Too many states ({len(machine.states)}) for {nb_bits} bits (max{max_states})")
+	
 	dico_states = {}
-	dico_states[machine.init] = "0"
-	dico_states[machine.accept] = "1"
+	dico_states[machine.init] = binary_conversion(0, nb_bits)
+	dico_states[machine.accept] = binary_conversion(1, nb_bits)
 
 	counter = 2
 
 	for state in machine.states :
 		if state not in dico_states:
-			dico_states[state] = conversion_binaire(counter)
+			dico_states[state] = binary_conversion(counter, nb_bits)
 			counter+=1
 	
 	return(dico_states)
 
-def conversion_binaire_alphabet(symbol):
-	return conversion_binaire(ord(symbol))
+def conversion_binaire_alphabet(symbol, nb_bits = 8):
+	return binary_conversion(ord(symbol), nb_bits)
 
-def encode_alphabet(machine):
+def encode_alphabet(machine, nb_bits = 8):
 	alphabet = set()
 
 	for key,value in machine.transitions.items():
 		alphabet.add(key[1])
 		alphabet.add(value[1][0])
 
+	max_symbols = 2**nb_bits
+	if len(alphabet)>max_symbols : 
+		raise ValueError(f"Too many symbols ({len(alphabet)} for {nb_bits} bits (max {max_symbols}))")
+	
 	alphabet_bis = {}
 
 	for symbol in alphabet :
-		alphabet_bis[symbol] = conversion_binaire_alphabet(symbol)
+		alphabet_bis[symbol] = conversion_binaire_alphabet(symbol, nb_bits)
 	return alphabet_bis
 
+MOVEMENTS = {'>' : 0, '-' : 1, '<':2 }
 
-def encode_transition(machine):
+def encode_movement(movement, nb_bits = 8):
+	if movement not in MOVEMENTS:
+		raise ValueError(f"Uknown movement :{movement}")
+	return binary_conversion(MOVEMENTS[movement], nb_bits)
+
+
+def encode_transition(machine, state_bits = 8, alphabet_bits = 8):
 	"""Transforms the syntax of the MT transitions into the syntax wanted
 	Parameter : MT"""
-	states = rename_states(machine)
-	alphabet_machine = encode_alphabet(machine)
+
+	states = rename_states(machine, state_bits)
+	alphabet_machine = encode_alphabet(machine, alphabet_bits)
 	t_transitions = []
 	
 	for key,value in machine.transitions.items() :
 		current_state = states[key[0]]
-		symbol_read = str(alphabet_machine[key[1]])
-
+		symbol_read = alphabet_machine[key[1]]
 		new_state = states[value[0]]
-		symbol_written = str(alphabet_machine[value[1][0]])
+		symbol_written = alphabet_machine[value[1][0]]
 		movement = value[2][0]
 		t_transitions.append(current_state + "|" + symbol_read + "|" + symbol_written + "|" + movement + "|" + new_state)
 
 	return "|".join(t_transitions)
 
-def MU(filepath):
+def universal_machine(filepath):
 	"""Final function to determine a universal machine 
 	Parameter : filepath"""
 	machine = load_from_file(filepath)
@@ -145,19 +161,22 @@ def MU(filepath):
 
 #print(MU("./files/test_1tape.tm"))
 
-def encode_binary(filepath):
+def encode_binary(filepath, nb_bits = 8):
 	"""Function that produces the binary coding of the mt simulator file 
 	Parameter : filepath"""
-	machine_final = MU(filepath)
+	machine_final = universal_machine(filepath)
 
 	encoding = []
 
 	for symbol in machine_final : 
-		encoding.append(conversion_binaire(ord(symbol)))
-		
-	encoding = "".join(encoding)
+		encoding.append(binary_conversion(ord(symbol), nb_bits))
 
+	encoding = "".join(encoding)
 	integer = int(encoding, 2)
+
+	utm_path = filepath.replace('.tm', '.utm')
+	with open(utm_path, 'w') as f: 
+		f.write(encoding)
 
 	return encoding, integer
 
